e2ac62a492090e58d554695afab19754645596a3
wveik <kataev_06@list.ru>
2016-03-04 15:02:41 +0300

    Первый commit

---
+++ ParserHTML/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ParserHTML {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtContains = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.dataGridResult = new System.Windows.Forms.DataGridView();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Link = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.Key = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timerMain = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStart.Location = new System.Drawing.Point(12, 344);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(142, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Запустить робота";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtContains
+            // 
+            this.txtContains.Location = new System.Drawing.Point(77, 50);
+            this.txtContains.Name = "txtContains";
+            this.txtContains.Size = new System.Drawing.Size(159, 20);
+            this.txtContains.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Contains";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Стоимость";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(77, 81);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(159, 20);
+            this.txtPrice.TabIndex = 5;
+            this.txtPrice.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrice_KeyPress);
+            // 
+            // dataGridResult
+            // 
+            this.dataGridResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridResult.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Title,
+            this.Price,
+            this.Link,
+            this.Key});
+            this.dataGridResult.Location = new System.Drawing.Point(12, 117);
+            this.dataGridResult.Name = "dataGridResult";
+            this.dataGridResult.Size = new System.Drawing.Size(487, 221);
+            this.dataGridResult.TabIndex = 6;
+            this.dataGridResult.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridResult_CellContentClick);
+            // 
+            // Title
+            // 
+            this.Title.HeaderText = "Title";
+            this.Title.Name = "Title";
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "Price";
+            this.Price.Name = "Price";
+            // 
+            // Link
+            // 
+            this.Link.HeaderText = "Link";
+            this.Link.Name = "Link";
+            this.Link.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Link.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Link.Width = 300;
+            // 
+            // Key
+            // 
+            this.Key.HeaderText = "Key";
+            this.Key.Name = "Key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "URL";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(77, 15);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(159, 20);
+            this.txtURL.TabIndex = 8;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "AVITO Parser";
+            this.notifyIcon1.Visible = true;
+            // 
+            // timerMain
+            // 
+            this.timerMain.Interval = 30000;
+            this.timerMain.Tick += new System.EventHandler(this.timerMain_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 379);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridResult);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtContains);
+            this.Controls.Add(this.btnStart);
+            this.Name = "MainForm";
+            this.Text = "Парсер";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtContains;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.DataGridView dataGridResult;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewLinkColumn Link;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Key;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timerMain;
+    }
+}
+
---
+++ ParserHTML/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParserHTML {
+    public partial class MainForm : Form {
+
+        public MainForm() {
+            InitializeComponent();
+
+            notifyIcon1.Visible = false;
+            this.notifyIcon1.MouseDoubleClick += new MouseEventHandler(notifyIcon1_MouseDoubleClick);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e) {
+            Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private string GetHTML(string urlAddress) {
+            string result = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK) {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null) {
+                    readStream = new StreamReader(receiveStream);
+                } else {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                result = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+            }
+
+            return result;
+        }
+
+        private HtmlDocument GetHtmlDocument(string html) {
+            WebBrowser browser = new WebBrowser();
+            browser.ScriptErrorsSuppressed = true;
+            browser.DocumentText = html;
+            browser.Document.OpenNew(true);
+            browser.Document.Write(html);
+            browser.Refresh();
+            return browser.Document;
+        }
+
+        private List<HtmlElement> GetListElementsByClass(HtmlElementCollection list, string className) {
+            List<HtmlElement> result = new List<HtmlElement>();
+            foreach (HtmlElement e in list)
+                if (e.GetAttribute("className") == className)
+                    result.Add(e);
+            return result;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e) {
+            StartOrStopTime();
+        }
+
+        private void StartOrStopTime() {
+            if (timerMain.Enabled) {
+                timerMain.Enabled = false;
+                btnStart.Text = "Запустить робота";
+            } else {
+                timerMain.Enabled = true;
+                btnStart.Text = "Остановить робота";
+                GetData();
+            }
+        }
+
+        private void GetData() {
+            List<List<string>> result = new List<List<string>>();
+
+            btnStart.Enabled = false;
+
+            int key = 0;
+            foreach (DataGridViewRow row_data_view in dataGridResult.Rows) {
+                if (row_data_view.Cells["Key"].Value != null && !string.IsNullOrEmpty(row_data_view.Cells["Key"].Value.ToString())) {
+                    int max = int.Parse(row_data_view.Cells["Key"].Value.ToString());
+                    if (key < max) key = max;
+                }
+            }
+
+            string url = txtURL.Text;
+            double disered_price = double.Parse(txtPrice.Text);
+
+            string data = GetHTML(url);
+
+            var doc = GetHtmlDocument(data);
+
+            var list = GetListElementsByClass(doc.All, "description");
+
+            foreach (var item in list) {
+                var href = item.GetElementsByTagName("a");
+                var inner_doc = GetHtmlDocument(item.InnerHtml);
+
+                var list_a = GetListElementsByClass(inner_doc.All, "item-description-title-link");
+                if (list_a.Count > 0 && list_a[0].InnerText.ToLower().Contains(txtContains.Text.ToLower())) {
+                    var price_list = GetListElementsByClass(inner_doc.All, "about");
+                    if (price_list.Count > 0 && !string.IsNullOrEmpty(price_list[0].InnerText)) {
+                        double price_page = double.Parse(price_list[0].InnerText.Replace("руб.", "").Replace(" ", ""));
+
+                        if (price_page <= disered_price) {
+                            string before = "https://www.avito.ru";
+                            string[] row = {
+                                list_a[0].InnerText.ToString(),
+                                price_list[0].InnerText,
+                                before + list_a[0].GetAttribute("href").ToString().Replace("about:", ""),
+                                ""
+                            };
+
+                            bool flag = true;
+
+                            foreach (DataGridViewRow row_data_view in dataGridResult.Rows) {
+                                if (row_data_view.Cells[0].Value != null &&
+                                    row_data_view.Cells[1].Value != null &&
+                                    row_data_view.Cells[2].Value != null) {
+
+                                    if (row_data_view.Cells[0].Value.ToString() == row[0] &&
+                                        row_data_view.Cells[1].Value.ToString() == row[1] &&
+                                        row_data_view.Cells[2].Value.ToString() == row[2]) {
+
+                                        flag = false;
+                                        break;
+                                    }
+                                }
+                            }
+                            if (flag) {
+                                key++;
+                                row[3] = key.ToString();
+                                dataGridResult.Rows.Add(row);
+                                result.Add(row.ToList());
+                            }
+                        }
+                    }
+                }
+                dataGridResult.Sort(dataGridResult.Columns["Key"], ListSortDirection.Descending);
+            }
+
+            StringBuilder builder = new StringBuilder();
+            foreach (var row in result) {
+                builder.Append(row[1] + " " + row[0]);
+                builder.Append(Environment.NewLine);
+            }
+
+            if(result.Count > 0)
+                notifyIcon1.ShowBalloonTip(5000, "Нашел " + txtContains.Text, builder.ToString(), ToolTipIcon.Info);
+
+            btnStart.Enabled = true;
+        }
+
+        private void txtPrice_KeyPress(object sender, KeyPressEventArgs e) {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8))
+                e.Handled = true;
+            if (e.KeyChar == 44) {
+                e.Handled = false;
+                foreach (char item in txtPrice.Text) {
+                    if (44 == item) {
+                        e.Handled = true;
+                    }
+                }
+            }
+        }
+
+        private void dataGridResult_CellContentClick(object sender, DataGridViewCellEventArgs e) {
+            if (e.ColumnIndex == dataGridResult.Columns["Link"].Index && e.RowIndex != -1) {
+                string url = dataGridResult.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+                Process.Start(url);
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e) {
+            if (WindowState == FormWindowState.Minimized) {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void timerMain_Tick(object sender, EventArgs e) {
+            GetData();
+        }
+    }
+}
